4a31c4289095dbd2da041d508bf5b14cd6192ab6
opbykyd <opbykyd@gmail.com>
2020-06-29 02:31:37 +0900

    Update kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -48,7 +48,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "battPercOff" not in self.config:
-        self.config.update({"battPercOff":"100"})
+        self.config.update({"battPercOff":"10"})
         self.config.update({"carVoltageMinEonShutdown":"11800"})
         self.config.update({"brakeStoppingTarget":"0.25"})
         self.element_updated = True
@@ -73,7 +73,7 @@ class kegman_conf():
         self.element_updated = True
 	
       if "deadzone" not in self.config:
-        self.config.update({"deadzone":"2.0"})
+        self.config.update({"deadzone":"10.0"})
         self.element_updated = True
 	
       if "1barBP0" not in self.config:
@@ -114,7 +114,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "ALCnudgeLess" not in self.config:
-        self.config.update({"ALCnudgeLess":"1"})
+        self.config.update({"ALCnudgeLess":"0"})
         self.config.update({"ALCminSpeed":"16.666667"})
         self.element_updated = True
 
@@ -136,15 +136,15 @@ class kegman_conf():
 
     else:
       self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
-                     "wheelTouchSeconds":"86400", "battPercOff":"100", "carVoltageMinEonShutdown":"11800", \
+                     "wheelTouchSeconds":"30000", "battPercOff":"10", "carVoltageMinEonShutdown":"11800", \
                      "brakeStoppingTarget":"0.25", "tuneGernby":"1", \
-                     "Kp":"-1", "Ki":"-1", "liveParams":"1", "leadDistance":"5", "deadzone":"2.0", \
+                     "Kp":"-1", "Ki":"-1", "liveParams":"1", "leadDistance":"5", "deadzone":"10.0", \
                      "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.1", "2barBP1":"2.5", "3barBP0":"0.0", \
                      "3barBP1":"3.0", "1barMax":"2.1", "2barMax":"2.1", "3barMax":"2.1", \
                      "1barHwy":"0.4", "2barHwy":"0.3", "3barHwy":"0.1", \
                      "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", "Kf":"-1", \
                      "sR_boost":"0", "sR_BP0":"0", "sR_BP1":"0", "sR_time":"0.2", \
-                     "ALCnudgeLess":"1", "ALCminSpeed":"16.666667", "ALCtimer":"1.0", "CruiseDelta":"8", \
+                     "ALCnudgeLess":"0", "ALCminSpeed":"16.666667", "ALCtimer":"1.0", "CruiseDelta":"8", \
                      "CruiseEnableMin":"0"}
 
 
